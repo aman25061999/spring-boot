69ef31526a09c3c50063c61ddf37b5f0d8c91212
aman25061999 <amansinha.sinha4924@gmail.com>
2022-07-30 13:30:11 +0530

    minor changes

---
+++ src/main/java/com/springboot/demo/SpringbokDemoApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringbokDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbokDemoApplication.class, args);
+		System.out.println("hello aman");
+	}
+
+}
